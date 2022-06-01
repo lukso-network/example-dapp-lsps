7c85f9ccb427c7198d392e228f32c52ff68200f7
Hugo Masclet <hugo@lukso.io>
2022-06-01 16:38:20 +0200

    chore: fix baseUrl and env loading

--- src/router.js
+++ src/router.js
@@ -1,34 +1,31 @@
-import {
-    createWebHistory,
-    createRouter
-} from "vue-router";
+import { createWebHistory, createRouter } from "vue-router";
 
 import Login from "./pages/Login.vue";
 import Dashboard from "./pages/Dashboard.vue";
 import CreateToken from "./pages/CreateToken.vue";
 // import CreateNFTCollection from "./pages/CreateNFTCollection.vue";
 
-const history = createWebHistory();
+const history = createWebHistory(import.meta.env.BASE_URL);
 const routes = [
-    {
-        path: "/",
-        component: Dashboard
-    },
-    {
-        path: "/login",
-        component: Login
-    },
-    {
-        path: "/createToken",
-        component: CreateToken
-    },
-    // {
-    //     path: "/createNFTCollection",
-    //     component: CreateNFTCollection
-    // },
+  {
+    path: "/",
+    component: Dashboard,
+  },
+  {
+    path: "/login",
+    component: Login,
+  },
+  {
+    path: "/createToken",
+    component: CreateToken,
+  },
+  // {
+  //     path: "/createNFTCollection",
+  //     component: CreateNFTCollection
+  // },
 ];
 const router = createRouter({
-    history,
-    routes
+  history,
+  routes,
 });
 export default router;
