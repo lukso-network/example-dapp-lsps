8f82141eea2d17c952b21d0e49d1db2f5ddadbc5
Fabian Vogelsteller <fabian@frozeman.de>
2022-06-14 11:21:37 +0200

    changed createToken

--- src/pages/CreateToken.vue
+++ src/pages/CreateToken.vue
@@ -23,142 +23,132 @@ export default {
   async mounted() {
 
 
-    const accounts = await ethereum.request({ method: "eth_accounts" });
+const accounts = await web3.eth.getAccounts()
 
 
     const newAsset = '0x64B5F0647227Fe99cC310FA633be5F946A623474'
 
-    // ADD creations to UP
-    // const options = {
-    //   ipfsGateway: 'https://2eff.lukso.dev/ipfs/' // todo the gateway should be without /ipfs/
-    // }
+// ADD creations to UP
+          // const options = {
+          //   ipfsGateway: 'https://2eff.lukso.dev/ipfs/' // todo the gateway should be without /ipfs/
+          // }
 
-    // // TODO fix  with new erc725.js
-    // const profile = new ERC725js(LSP3UniversalProfileMetaDataSchema, accounts[0], window.web3.currentProvider, options)
-    // // const asset = new ERC725js(LSP4DigitalAssetMetaDataSchema, contractAddress, window.web3.currentProvider, options)
-    // // console.log(await asset.fetchData('LSP4Metadata'))
+          // // TODO fix  with new erc725.js
+          // const profile = new ERC725js(LSP3UniversalProfileMetaDataSchema, accounts[0], window.web3.currentProvider, options)
+          // // const asset = new ERC725js(LSP4DigitalAssetMetaDataSchema, contractAddress, window.web3.currentProvider, options)
+          // // console.log(await asset.fetchData('LSP4Metadata'))
 
-    // // GET the current issued assets
-    // const LSP3IssuedAssets = await profile.getData('LSP3IssuedAssets[]')
+          // // GET the current issued assets
+          // const LSP3IssuedAssets = await profile.getData('LSP3IssuedAssets[]')
 
-    // // add new asset
-    // LSP3IssuedAssets.push(newAsset)
-    // console.log(LSP3IssuedAssets)
+          // // add new asset
+          // LSP3IssuedAssets.push(newAsset)
+          // console.log(LSP3IssuedAssets)
 
-    // const newData = profile.encodeData({ // TODO change to ERC725js
-    //   'LSP3IssuedAssets[]': LSP3IssuedAssets,
-    //   // 'LSP3IssuedAssetsMap:<address>': {
-    //     //   dynamicKeyParts: newAsset,
-    //   //   value: web3.utils.leftPad(LSP3IssuedAssets.length - 1, 16) + 'e33f65c3' // index position of asset + LSP7 interface ID: 0xe33f65c3
-    //   // }
-    // });
+          // const newData = profile.encodeData({ // TODO change to ERC725js
+          //   'LSP3IssuedAssets[]': LSP3IssuedAssets,
+          //   'LSP3IssuedAssetsMap:<address>': {
+          //       dynamicKeyParts: newAsset,
+          //     value: web3.utils.leftPad(LSP3IssuedAssets.length - 1, 16) + 'e33f65c3' // index position of asset + LSP7 interface ID: 0xe33f65c3
+          //   }
+          // });
 
-    // // add asset map key and value
-    // // construct asset map key
-    // const LSP3IssuedAssetsMapKey = ERC725js.encodeKeyName('LSP3IssuedAssetsMap:' + newAsset.replace('0x', '')); // TODO use instance
-    // newData.keys.push(LSP3IssuedAssetsMapKey)
-    // newData.values.push(web3.utils.leftPad(LSP3IssuedAssets.length - 1, 16) + 'e33f65c3') // index position of asset + LSP7 interface ID: 0xe33f65c3
+          // // add asset map key and value
+          // // construct asset map key
+          // const LSP3IssuedAssetsMapKey = ERC725js.encodeKeyName('LSP3IssuedAssetsMap:' + newAsset.replace('0x', '')); // TODO use instance
+          // newData.keys.push(LSP3IssuedAssetsMapKey)
+          // newData.values.push(web3.utils.leftPad(LSP3IssuedAssets.length - 1, 16) + 'e33f65c3') // index position of asset + LSP7 interface ID: 0xe33f65c3
 
-    // console.log(newData)
-
+          // console.log(newData)
+    
   },
 
   methods: {
     async onSubmit(e) {
 
       // GET the address from the browser extension
-      const accounts = await ethereum.request({ method: "eth_accounts" });
+      const accounts = await web3.eth.getAccounts()
 
       // CONSTRUCT the meta data
       const LSP4MetaData = {
         // "LSP4Metadata": { // TODO uncomment
-        "description": e.target.querySelector('textarea#description').value,
-        "icon": e.target.querySelector('input#icon').files[0],
-        "links": [],
-        "images": [],
-        "assets": []
+            "description": e.target.querySelector('textarea#description').value,
+            "icon": e.target.querySelector('input#icon').files[0],
+            "links": [],
+            "images": [],
+            "assets": []
         // }
-      };
+      }
 
       // INITIATE the LSPFactory
-      const factory = new LSPFactory(web3.currentProvider);
+      const factory = new LSPFactory(web3.currentProvider)
 
 
 
       // show the deploying status...
-      this.deployEvents = [];
-      this.deploying = true;
+      this.deployEvents = []
+      this.deploying = true
 
-
+      
       // DEPLOY the LSP7 token
-      factory.LSP7DigitalAsset.deploy({
-        name: e.target.querySelector('input#name').value,
+      await factory.LSP7DigitalAsset.deploy({
+        name: e.target.querySelector('input#name').value, 
         symbol: e.target.querySelector('input#symbol').value,
         controllerAddress: accounts[0], // the "issuer" of the asset, that is allowed to change meta data
         creators: [accounts[0]], // Array of ERC725Account addresses that define the creators of the digital asset.
         isNFT: false, // Token decimals set to 18
         digitalAssetMetadata: LSP4MetaData
-      }, {
-        deployReactive: true,
-        ipfsGateway: 'https://api.2eff.lukso.dev:443'
-      }).subscribe({
-        next: (deploymentEvent) => {
-          console.log(deploymentEvent);
+      },{
+        ipfsGateway: 'https://api.2eff.lukso.dev:443',
+        onDeployEvents: {
+          next: (deploymentEvent) => {
 
-          if (deploymentEvent.status === "COMPLETE")
-            this.deployEvents.push(deploymentEvent);
-        },
-        error: (error) => {
-          this.deploying = false;
-          this.error = error.message;
-        },
-        complete: async (contracts) => {
-          this.deploying = false;
+            console.log(deploymentEvent)
 
-          if (!contracts || !contracts.LSP7DigitalAsset) {
-            this.error = 'Error deploying LSP7DigitalAsset';
-            return;
+            if(deploymentEvent.status === "COMPLETE")
+              this.deployEvents.push(deploymentEvent);
+          },
+          error: (error) => {
+            this.deploying = false
+            this.error = error.message
+          },
+          complete: async (contracts) => {
+            this.deploying = false
           }
-
-          // ADD creations to UP
-          const options = {
-            ipfsGateway: 'https://2eff.lukso.dev/ipfs/' // todo the gateway should be without /ipfs/
-          };
-
-          // TODO fix  with new erc725.js
-          const profile = new ERC725js(LSP3UniversalProfileMetaDataSchema, accounts[0], window.web3.currentProvider, options);
-          const asset = new ERC725js(LSP4DigitalAssetMetaDataSchema, contracts.LSP7DigitalAsset.address, window.web3.currentProvider, options);
-          console.log('ASSET', await asset.fetchData('LSP4Metadata'));
-
-          // GET the current issued assets
-          const LSP3IssuedAssets = await profile.getData('LSP3IssuedAssets[]'); //TODO change to LSP12IssuedAssets[]
-
-          // add new asset
-          LSP3IssuedAssets.push(newAsset);
-          console.log(LSP3IssuedAssets);
-
-          const newData = profile.encodeData({ // TODO change to ERC725js
-            'LSP3IssuedAssets[]': LSP3IssuedAssets,
-            // 'LSP3IssuedAssetsMap:<address>': {
-            //   dynamicKeyParts: newAsset,
-            //   value: web3.utils.leftPad(LSP3IssuedAssets.length - 1, 16) + 'e33f65c3' // index position of asset + LSP7 interface ID: 0xe33f65c3
-            // }
-          });
-
-          // add asset map key and value
-          // construct asset map key
-          const LSP3IssuedAssetsMapKey = ERC725js.encodeKeyName('LSP3IssuedAssetsMap:' + newAsset.replace('0x', '')); // TODO use instance; change to LSP12IssuedAssetsMap
-          newData.keys.push(LSP3IssuedAssetsMapKey);
-          newData.values.push(web3.utils.leftPad(LSP3IssuedAssets.length - 1, 16) + 'e33f65c3'); // index position of asset + LSP7 interface ID: 0xe33f65c3
-
-          console.log(newData);
-
-          // send transaction
-          const profileContract = new window.web3.eth.Contract(LSP0ERC725Account.abi, accounts[0]);
-          await profileContract.methods.setData(newData.keys, newData.values).send({ from: accounts[0] });
-
-        },
+        }
       });
+
+      if(!contracts || !contracts.LSP7DigitalAsset) {
+        this.error = 'Error deploying LSP7DigitalAsset'
+        return
+      }
+      
+      // ADD creations to UP
+        const options = {
+        ipfsGateway: 'https://2eff.lukso.dev/ipfs/' // todo the gateway should be without /ipfs/
+      }
+
+      const profile = new ERC725js(LSP3UniversalProfileMetaDataSchema, accounts[0], window.web3.currentProvider, options)
+
+      // GET the current issued assets
+      const LSP3IssuedAssets = await profile.getData('LSP3IssuedAssets[]')
+
+      // add new asset
+      LSP3IssuedAssets.push(newAsset)
+
+      const newData = profile.encodeData({
+        'LSP3IssuedAssets[]': LSP3IssuedAssets,
+        'LSP3IssuedAssetsMap:<address>': {
+          dynamicKeyParts: newAsset,
+          value: '0xe33f65c3' + web3.utils.leftPad(LSP3IssuedAssets.length - 1, 16).slice(2) // index position of asset + LSP7 interface ID: 0xe33f65c3
+        }
+      });
+
+      console.log(newData)
+
+      // send transaction
+      const profileContract = new window.web3.eth.Contract(LSP0ERC725Account.abi, accounts[0])
+      await profileContract.methods.setData(newData.keys, newData.values).send({from: accounts[0]})
     }
   }
 
@@ -168,10 +158,10 @@ export default {
 
 <template>
 
-  <a class="back" @click="$router.push('/')">&lt;</a>
+  <a class="back" href="/">&lt;</a>
 
   <p class="warning" v-if="error">
-    {{ error }}
+    {{error}}
   </p>
 
   <div class="center">
@@ -180,7 +170,8 @@ export default {
       based on <a href="https://docs.lukso.tech/standards/nft-2.0/LSP7-Digital-Asset" target="_blank">LSP7</a>
     </h2>
 
-    <br><br>
+    <br>
+    <br>
 
     <form v-if="!deploying" @submit.prevent="onSubmit" class="left">
       <fieldset>
@@ -197,7 +188,7 @@ export default {
         <input type="file" id="icon" accept="image/*" required>
 
         <br><br>
-
+        
         <input class="button-primary" type="submit" value="Create Token">
       </fieldset>
     </form>
@@ -206,27 +197,21 @@ export default {
 
   <div class="events">
     <span v-if="deploying">
-      Deploying Smart Contracts..<br>
-      <strong>Please confirm all transactions in your browser extension, and wait until they are added to the
-        Blockchain.</strong>
+        Deploying Smart Contracts..<br>
+        <strong>Please confirm all transactions in your browser extension, and wait until they are added to the Blockchain.</strong>
     </span>
 
     <br><br>
 
     <div v-for="(event, index) in deployEvents" :key="index">
       <span v-if="event.type === 'PROXY_DEPLOYMENT'">
-        Contract deployed: {{ event.contractName }} ({{ event.type }}): <a
-          :href="'https://blockscout.com/lukso/l14/address/' + event.contractAddress" target="_blank">{{
-              event.contractAddress
-          }}</a><br>
-        Transaction hash: <a :href="'https://blockscout.com/lukso/l14/tx/' + event.receipt.transactionHash"
-          target="_blank">{{ event.receipt.transactionHash }}</a>
+        Contract deployed: {{ event.contractName }} ({{ event.type }}): <a :href="'https://blockscout.com/lukso/l14/address/' + event.contractAddress" target="_blank">{{event.contractAddress}}</a><br>
+        Transaction hash: <a :href="'https://blockscout.com/lukso/l14/tx/' + event.receipt.transactionHash" target="_blank">{{event.receipt.transactionHash}}</a>
       </span>
       <br>
       <span v-if="event.type === 'TRANSACTION'">
         Function called: {{ event.functionName }}()<br>
-        Transaction hash: <a :href="'https://blockscout.com/lukso/l14/tx/' + event.receipt.transactionHash"
-          target="_blank">{{ event.receipt.transactionHash }}</a>
+        Transaction hash: <a :href="'https://blockscout.com/lukso/l14/tx/' + event.receipt.transactionHash" target="_blank">{{event.receipt.transactionHash}}</a>
       </span>
     </div>
   </div>
--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -7,12 +7,15 @@ export default {
 
   data() {
     return {
+      createdAssets: [],
+      ownedAssets: []
     }
   },
 
   // Executed when the login page is rendered
   async mounted() {
 
+      // READ LSP12 and LSP5
 
   },
 
@@ -30,11 +33,14 @@ export default {
 <template>
 
   <div class="center">
+
     <ProfileComponent />
 
     <button @click="$router.push('/createToken')">Create a Token</button>
     <!--TODO: add onclick after router is working-->
     <button>Create an NFT Collection</button>
+
+
   </div>
 
 </template>
