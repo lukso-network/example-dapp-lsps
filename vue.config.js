a7983017d9df18a44d85ba75fb20f9a1dbc46033
Hugo Masclet <hugo@lukso.io>
2022-06-15 17:08:44 +0200

    Fix publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,26 @@
-const webpack = require("webpack");
-const { defineConfig } = require("@vue/cli-service");
+const webpack = require('webpack');
+const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
+  publicPath: process.env.BASE_URL,
   transpileDependencies: true,
   configureWebpack: {
     plugins: [
       new webpack.ProvidePlugin({
-        Buffer: ["buffer", "Buffer"],
-        process: "process/browser",
+        Buffer: ['buffer', 'Buffer'],
+        process: 'process/browser',
       }),
     ],
     resolve: {
       fallback: {
         assert: false,
-        buffer: require.resolve("buffer"),
-        stream: require.resolve("stream-browserify"),
-        crypto: require.resolve("crypto-browserify"),
-        http: require.resolve("stream-http"),
-        https: require.resolve("https-browserify"),
-        url: require.resolve("url/"),
-        os: require.resolve("os-browserify/browser"),
+        buffer: require.resolve('buffer'),
+        stream: require.resolve('stream-browserify'),
+        crypto: require.resolve('crypto-browserify'),
+        http: require.resolve('stream-http'),
+        https: require.resolve('https-browserify'),
+        url: require.resolve('url/'),
+        os: require.resolve('os-browserify/browser'),
       },
     },
   },
